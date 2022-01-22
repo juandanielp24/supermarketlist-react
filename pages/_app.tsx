946274f36ff3ea648d6333ecb0d13ecdaf4970be
Juan Daniel Perez <juandanielp24@gmail.com>
2022-01-22 19:37:33 -0300

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import {ChakraProvider, Box, Container, VStack, Heading, Text} from "@chakra-ui/react";
+import type {AppProps} from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import theme from "../theme";
+
+function App({Component, pageProps}: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box>
+        <Container
+          backgroundColor="white"
+          borderRadius="sm"
+          boxShadow="md"
+          maxWidth="container.sm"
+          padding={4}
+        >
+          <VStack>
+            <Heading>Supermarket list</Heading>
+          </VStack>
+          <Component {...pageProps} />
+        </Container>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default App;
