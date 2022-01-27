bc187db466be950efb465e057a44a765ce00befd
Juan Daniel Perez <juandanielp24@gmail.com>
2022-01-27 12:24:59 -0300

    add header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,36 @@
 import * as React from "react";
 import {ChakraProvider, Box, Container, VStack, Heading, Text} from "@chakra-ui/react";
 import type {AppProps} from "next/app";
+import Head from "next/head";
 
 import theme from "../theme";
 
 function App({Component, pageProps}: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Box>
-        <Container
-          backgroundColor="white"
-          borderRadius="sm"
-          boxShadow="md"
-          maxWidth="container.sm"
-          padding={4}
-        >
-          <VStack>
-            <Heading>Supermarket list</Heading>
-          </VStack>
-          <Component {...pageProps} />
-        </Container>
-      </Box>
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Supermarket List</title>
+        <meta content="initial-scale=1.0, width=device-width" name="viewport" />
+        <meta content="Juan Daniel Perez" name="author" />
+        <meta content="Juan Daniel Perez" name="copyright" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Box>
+          <Container
+            backgroundColor="white"
+            borderRadius="sm"
+            boxShadow="md"
+            maxWidth="container.sm"
+            padding={4}
+          >
+            <VStack>
+              <Heading>Supermarket list</Heading>
+            </VStack>
+            <Component {...pageProps} />
+          </Container>
+        </Box>
+      </ChakraProvider>
+    </>
   );
 }
 
